a3b4bfc63f99a54700cde2f2aad6dd6ef361a27a
Vangeance <ismaelmartins919@gmail.com>
2022-08-16 01:41:12 -0300

    segunda versao

---
+++ main.js
@@ -0,0 +1,52 @@
+// SLIDE
+
+const imgs = document.querySelector(".home-slides");
+const img = document.querySelectorAll(".home-slides li");
+
+let idx = 0;
+
+function carrosel(){
+    idx++;
+
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 65}vw)`;
+}
+
+setInterval(carrosel, 4000);
+
+// Contatos Modal
+
+const $cards = document.querySelectorAll(".js-card");
+const $menu = document.querySelector(".js-menu");
+const sectionToggleClass = "js-section-toggle";
+const $menuToggles = $menu.querySelectorAll(`.${sectionToggleClass}`);
+
+$menu.addEventListener("click", (e) => {
+  if (e.target.classList.contains(sectionToggleClass)) {
+    activateCard(e.target.parentElement);
+    markAsActiveMenuItem(e.target);
+  }
+});
+
+function activateCard(el) {
+  const socialnetwork = el.dataset.socialNetwork;
+
+  $cards.forEach(($card) => {
+    $card.classList.remove("is-active");
+
+    if ($card.classList.value.includes(socialnetwork)) {
+      $card.classList.add("is-active");
+    }
+  });
+}
+
+function markAsActiveMenuItem(el) {
+  $menuToggles.forEach(($item) => {
+    $item.classList.remove("is-active");
+  });
+
+  el.classList.add("is-active");
+}
